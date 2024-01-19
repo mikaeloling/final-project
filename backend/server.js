288f386473bf480ee46d9503a3aca1988d77916b
“mikaeloling” <mikael.oling@gmail.com>
2024-01-19 23:11:59 +0200

    db live connection

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ import { connectDB } from "./config/db";
 const port = process.env.PORT || 3000;
 const app = express(); 
 
+mongoose.connect(process.env.MONGO_URL);
+
 app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: false })); 
